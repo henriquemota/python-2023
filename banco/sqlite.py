d2b41faf960ad26456c66c2d8f4bec5083cedcee
henriquemota <mota.henrique@gmail.com>
2023-04-17 19:57:22 -0300

    inicializacao do banco de dados

---
+++ banco/sqlite.py
@@ -0,0 +1,26 @@
+import os
+import sqlite3 as conector
+
+dirname = os.path.dirname(__file__)
+url = os.path.join(dirname, 'data', 'mydb.db')
+
+def criar_banco():
+  try:
+    conexao = conector.connect(url)
+    cursor = conexao.cursor()
+
+    sql = (
+        'create table if not exists pessoas ('
+        'id int primary key,'
+        'nome varchar(100)'
+        ');'
+    )
+
+    cursor.execute(sql)
+  except conector.DatabaseError as err:
+    print('erro de banco de dados')
+  except:
+    print('erro')
+  finally:
+    cursor.close()
+    conexao.close()
